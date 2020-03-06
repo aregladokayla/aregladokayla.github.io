4ee4ac99b0b560ee32a798126fee06709712a14a
aregladokayla <aregk337@newschool.edu>
2020-03-06 18:02:16 -0500

    Update Fizzy-Water

--- week6/fizzy-water.js
+++ week6/fizzy-water.js
@@ -1,9 +1,47 @@
+//Positions hovered/popped bubbles
+function bubblePopped (event) {
+    let hoverObject = event.target;
+    if (event.propertyName == 'opacity') {
+        hoverObject.style.top = `${Math.round(Math.random() * 100)}%` ;
+        hoverObject.style.left = `${Math.round(Math.random() * 95)}%` ;
+
+        hoverObject.classList.remove('pop');
+        hoverObject.removeEventListener('transitionend', bubblePopped);
+    }
+}
+
+//Starting position for bubbles
 function popBubble (event) {
     let hoverObject = event.target;
     if (hoverObject.classList.contains('bubble')) {
         hoverObject.classList.add('pop');
+
+        hoverObject.addEventListener('transitionend', bubblePopped);
     }
 }
 
+function popBubbles(event) {
+    let bubbles = document.querySelectorAll('.bubble');
+    bubbles.forEach(function (bubble) {
+        bubble.classList.add('pop');
+        bubble.addEventListener('transitionend', bubblePopped);
+    });
+}
+
 let bubbleWrapper = document.querySelector('.bubble-wrapper');
-bubbleWrapper.addEventListener('mouseover', popBubble);
+let glass = document.querySelector('#glass');
+
+//bubbleWrapper.addEventListener('mouseover', popBubbles);
+glass.addEventListener('click', popBubbles);
+
+//Creates many bubbles
+for (let i = 0; i < 80; i++) {
+    //create divs in DOM
+    let bubble = document.createElement('div');
+    bubble.className = 'bubble';
+    bubble.style.top = `${Math.round(Math.random() * 100)}%`;
+    bubble.style.left = `${Math.round(Math.random() * 95)}%`;
+
+    //add divs to container
+    bubbleWrapper.append(bubble);
+}
